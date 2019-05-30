8d81300bdacd030105b5ee551a446dfa783e2c3a
copperface <copperface08091993@gmail.com>
2019-05-30 23:37:25 +0500

    Updates

---
+++ precache-manifest.fd59144ccfa68e51a45dd78fcba1bbfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17f8152d7d96104e57ffeeb7c502b23",
+    "url": "/film-note/index.html"
+  },
+  {
+    "revision": "99caeb0fbaa4dcc67c01",
+    "url": "/film-note/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e9911e795d7c5f584b3d",
+    "url": "/film-note/static/js/2.576496b6.chunk.js"
+  },
+  {
+    "revision": "99caeb0fbaa4dcc67c01",
+    "url": "/film-note/static/js/main.19bb129d.chunk.js"
+  },
+  {
+    "revision": "d20210b75e156bb23f2d",
+    "url": "/film-note/static/js/runtime~main.43dd844a.js"
+  }
+]);
